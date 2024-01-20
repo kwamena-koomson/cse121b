81b1441e94aec7f52f42e1f71721dc3a46f1becd
Kwamena Koomson <kwamenakooms@gmail.com>
2024-01-20 12:09:30 +0000

    Final work on week 2

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,27 +3,32 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-
-
+const fullName = 'Kwamena Koomson';
+const currentYear = new Date().getFullYear() ;
+const profilePicture = "images/kwamena.jpg";
 
 
 /* Step 3 - Element Variables */
-
-
-
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.getElementById('year'); 
+const imageElement = document.querySelector('img')
 
 
 /* Step 4 - Adding Content */
 
-
-
+yearElement.textContent = currentYear ;
+imageElement.setAttribute('src', profilePicture) ;
+imageElement.setAttribute('alt', `This is ${fullName}$. He is a student at BYU-Idaho`) ;
+nameElement.innerHTML = '<strong>' + fullName + '</strong>';
 
 
 
 /* Step 5 - Array */
+let favfoods = ['Jollof', 'Tuo Zaafi', 'Pizza', 'Banku'];
 
+foodElement.innerHTML = favfoods;
 
-
-
-
-
+let newfood = 'Croissant';
+favfoods.push(newfood) ;
+foodElement.innerHTML += `<br>${favfoods}`;
